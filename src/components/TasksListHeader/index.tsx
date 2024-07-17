32e46b5f2e3ff3bcf45eb89bfa24160d9b23d128
michaelmoranis <michaelmoranesul@gmail.com>
2024-07-17 06:51:32 -0300

    refactoring: src/components/TasksListHeader/index.tsx

--- src/components/TasksListHeader/index.tsx
+++ src/components/TasksListHeader/index.tsx
@@ -4,7 +4,7 @@ interface taskCompletedProps {
 
 export  function TaskListHeader ({taskCompleted}: taskCompletedProps ) {
     return (
-        <div className="flex gap-2 text-xl text-center text-zinc-700 px-2 bg-orange-400 rounded-md w-2/4">
+        <div className="flex gap-2 text-xl text-center text-zinc-700 px-2 ml-2 bg-orange-400 rounded-md w-72">
             tarefas concluidas:
             <span>{taskCompleted}</span>
         </div>
