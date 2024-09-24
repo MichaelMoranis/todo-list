702d0f0c40627e17913a1c34797445edb13f7f8d
michaelmoranis <michaelmoranesul@gmail.com>
2024-09-24 20:09:25 -0300

    fix: adicionando mensagem de carregamento dos dados

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { TodoListProps } from "./types";
 
 function App() {
   const [input, SetInput] = useState("");
+  const [loading, setLoading] = useState(true)
   const [valueItem, setValueItem] = useState<TodoListProps[]>([]);
   // aqui estou atualizando o localstorage sempre que "valueItem no [array] mudar"
 
@@ -17,67 +18,72 @@ function App() {
     SetInput(e.target.value);
   }
 
-async function listInput() {
-  try {
-    const response = await fetch ("https://todo-server-gdw2.onrender.com/tasks")
-    const dataTask = await response.json();
+  async function listInput() {
+    setLoading(true)
+    try {
+      const response = await fetch("https://todo-server-gdw2.onrender.com/tasks")
+      const dataTask = await response.json();
 
-    if(!response.ok) {
-      throw new Error(`erro: ${response.text}`)
+      if (!response.ok) {
+        throw new Error(`erro: ${response.text}`)
+      }
+      setValueItem(dataTask);
+    } catch (error) {
+      console.log("deu erro no get")
     }
-    setValueItem(dataTask);
-  } catch(error) {
-    console.log("deu erro no get")
+    finally {
+      setLoading(false)
+    }
   }
-}
   // funcao para adicionar elementos na lista
-async function addInput(newText: string) {
-  // Verifica se o texto não está vazio
-   if(newText.trim() === "") {
-    return
-   }
-  const newInput = 
+  async function addInput(newText: string) {
+    // Verifica se o texto não está vazio
+    if (newText.trim() === "") {
+      return
+    }
+    const newInput =
     {
-      newtext: newText, 
+      newtext: newText,
       isChecked: false
     }
-  ;
+      ;
 
-  try {
-    const response = await fetch("https://todo-server-gdw2.onrender.com/tasks", {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newInput)
-    });
+    try {
+      const response = await fetch("https://todo-server-gdw2.onrender.com/tasks", {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newInput)
+      });
 
-    if (!response.ok) {
-      throw new Error(`Erro: ${response.statusText}`);
+      if (!response.ok) {
+        throw new Error(`Erro: ${response.statusText}`);
+      }
+      SetInput(""); // Certifique-se de que SetInput esteja definido e faça sentido no contexto
+
+    } catch (error) {
+      console.error("Erro ao adicionar tarefas:", error);
     }
-    SetInput(""); // Certifique-se de que SetInput esteja definido e faça sentido no contexto
-
-  } catch (error) {
-    console.error("Erro ao adicionar tarefas:", error);
+    listInput()
   }
-  listInput()
-}
 
 
   // funcao para deletar itens na lista
-async function deleteItem(id: number) {
+  async function deleteItem(id: number) {
     try {
       const deleteTask = await fetch(`https://todo-server-gdw2.onrender.com/tasks/${id}`, {
         method: 'DELETE'
-      })   
+      })
 
-      if(!deleteTask.ok) {
+      if (!deleteTask.ok) {
         console.log("erro ao deletar")
       }
-      listInput()   
+
     } catch {
       console.log("erro ao deletar")
     }
+    listInput()
   }
 
   // funcao para atualizar o novo array de efeito do drag and drop
@@ -90,18 +96,25 @@ async function deleteItem(id: number) {
       <div className="flex flex-col items-center w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl">
         <Header input={input} handleInput={handleInput} addInput={addInput} />
         <div className="flex flex-col w-full rounded-md my-4">
-          <div className="placeholder:only:rounded-md">
-            <TodoList
-              valueItem={valueItem}
-              deleteItem={deleteItem}
-              updateItems={updateItems}
-            />
+          <div className="flex flex-col w-full rounded-md my-4">
+            {
+              loading ? (
+                <p className="font-bold text-orange-600 text-center">carregando...</p>
+              )
+                : (
+                  <TodoList
+                    valueItem={valueItem}
+                    deleteItem={deleteItem}
+                    updateItems={updateItems}
+                  />
+                )
+            }
           </div>
         </div>
       </div>
     </div>
   );
-  
+
 }
 
 export default App;
